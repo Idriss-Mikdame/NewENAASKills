0a8fee07e65613905839cd33f808d05d902b2d1b
Idriss-Mikdame <mikdameidriss420@gmail.com>
2025-07-17 11:37:46 +0100

    fix comptence

---
+++ CompetenceEtSouCompetence/src/main/java/ma/enaa/competenceetsoucompetence/Mapper/Competence.java
@@ -0,0 +1,61 @@
+package ma.enaa.competenceetsoucompetence.Mapper;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Competence {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+    private String nom;
+    private String description;
+    private boolean etatValidation;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "competence")
+    private List<SousCompetence> sousCompetences;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isEtatValidation() {
+        return etatValidation;
+    }
+
+    public void setEtatValidation(boolean etatValidation) {
+        this.etatValidation = etatValidation;
+    }
+
+    public List<SousCompetence> getSousCompetences() {
+        return sousCompetences;
+    }
+
+    public void setSousCompetences(List<SousCompetence> sousCompetences) {
+        this.sousCompetences = sousCompetences;
+    }
+}
---
+++ CompetenceEtSouCompetence/src/main/java/ma/enaa/competenceetsoucompetence/Mapper/SousCompetence.java
@@ -0,0 +1,57 @@
+package ma.enaa.competenceetsoucompetence.Mapper;
+
+import jakarta.persistence.*;
+
+@Entity
+public class SousCompetence {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Génération auto de l'ID
+    private Long id;
+    private String nom;
+    private String description;
+    private  boolean etatValidation;
+
+    @ManyToOne
+    @JoinColumn(name = "competence_id")
+    private Competence competence;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isEtatValidation() {
+        return etatValidation;
+    }
+
+    public void setEtatValidation(boolean etatValidation) {
+        this.etatValidation = etatValidation;
+    }
+
+    public Competence getCompetence() {
+        return competence;
+    }
+
+    public void setCompetence(Competence competence) {
+        this.competence = competence;
+    }
+}
